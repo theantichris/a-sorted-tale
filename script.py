cff32f6970c72c3b70c08b1d75ecbf81248186da
Christopher <chris@theantichris.com>
2020-01-09 07:14:05 -0500

    bubble sort by author ascending

--- script.py
+++ script.py
@@ -4,10 +4,16 @@ import sorts
 def by_title_ascending(book_a, book_b):
     return book_a["title_lower"] > book_b["title_lower"]
 
-bookshelf = utils.load_books("books_small.csv")
-# for book in bookshelf:
-#     print(book["title"])
+def by_author_ascending(book_a, book_b):
+    return book_a["author_lower"] > book_b["author_lower"]
 
-sort_1 = sorts.bubble_sort(bookshelf, by_title_ascending)
-for book in sort_1:
-    print(book["title"])
+bookshelf = utils.load_books("books_small.csv")
+
+# sort_1 = sorts.bubble_sort(bookshelf, by_title_ascending)
+# for book in sort_1:
+#     print("{} - {}".format(book["title"], book["author"]))
+
+bookshelf_v1 = bookshelf.copy()
+sort_2 = sorts.bubble_sort(bookshelf_v1, by_author_ascending)
+for book in sort_2:
+    print("{} - {}".format(book["title"], book["author"]))
